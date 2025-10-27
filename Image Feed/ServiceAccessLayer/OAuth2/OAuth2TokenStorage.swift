7c3085834cbf515b8e52056dd01d62ee4173d000
stpkch <kachusovstepa@gmail.com>
2025-10-28 00:07:25 +0500

    Внес правки

--- Image Feed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
+++ Image Feed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
@@ -1,5 +1,57 @@
 import Foundation
 
 final class OAuth2TokenStorage {
-    var token: String?
+    private let dataStorage =  UserDefaults.standard
+    private let tokenKey = "token"
+
+    var token: String? {
+        get {
+            dataStorage.string(forKey: tokenKey)
+        }
+        set {
+            if let token = newValue {
+                dataStorage.set(token, forKey: tokenKey)
+            } else {
+                dataStorage.removeObject(forKey: tokenKey)
+            }
+        }
+    }
 }
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+    case invalidRequest
+    case decodingError(Error)
+}
+
+extension URLSession {
+    func data(
+        for request: URLRequest,
+        completion: @escaping (Result<Data, Error>) -> Void
+    ) -> URLSessionTask {
+        let fulfillCompletionOnTheMainThread: (Result<Data, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+        
+        let task = dataTask(with: request, completionHandler: { data, response, error in
+            if let data = data, let response = response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if 200 ..< 300 ~= statusCode {
+                    fulfillCompletionOnTheMainThread(.success(data))
+                } else {
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error = error {
+                fulfillCompletionOnTheMainThread(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                fulfillCompletionOnTheMainThread(.failure(NetworkError.urlSessionError))
+            }
+        })
+        
+        return task
+    }
+}
+
